e3b34b268b525fd8a77c82386f548e54609e2075
Alireza Raei <alirezaraei17@gmail.com>
2024-02-19 18:25:22 +1100

    feat: Integrate Leaflet for map display
    - Include Leaflet CSS and JavaScript dependencies
    - Initialize Leaflet map centered on user location
    - Add OpenStreetMap tile layer with attribution
    - Place marker on the user's location with a popup

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ if (navigator.geolocation)
     position => {
       const { latitude: lat, longitude: lon } = position.coords;
       console.log(`https://www.google.com.au/maps/@${lat},${lon}`);
+
+      const map = L.map('map').setView([lat, lon], 13);
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([lat, lon])
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     () => {
       alert('Could not get your location!');
