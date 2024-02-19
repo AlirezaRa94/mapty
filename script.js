1d674462e1c7fe8d1b7b1bb5e49a2ec195dd282d
Alireza Raei <alirezaraei17@gmail.com>
2024-02-19 17:50:06 +1100

    feat: Implement basic geolocation functionality
    - Add geolocation code to retrieve user's latitude and longitude
    - Generate Google Maps link with coordinates
    - Handle error if geolocation is unavailable

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude: lat, longitude: lon } = position.coords;
+      console.log(`https://www.google.com.au/maps/@${lat},${lon}`);
+    },
+    () => {
+      alert('Could not get your location!');
+    }
+  );
