30fb7adc9377177128a84870b52524d422845f60
Alireza Raei <alirezaraei17@gmail.com>
2024-02-19 19:00:42 +1100

    feat: Add functionality to drop workout markers
    - Refactor geolocation variables for clarity
    - Implement click handler to add markers on the map
    - Create Leaflet popups for workout markers
    - Style popups for a custom look

--- script.js
+++ script.js
@@ -3,20 +3,30 @@
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     position => {
-      const { latitude: lat, longitude: lon } = position.coords;
-      console.log(`https://www.google.com.au/maps/@${lat},${lon}`);
+      const { latitude, longitude } = position.coords;
 
-      const map = L.map('map').setView([lat, lon], 13);
+      const map = L.map('map').setView([latitude, longitude], 13);
 
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([lat, lon])
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', mapEvent => {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     () => {
       alert('Could not get your location!');
